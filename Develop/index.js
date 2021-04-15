c8b313fe19c1947ef518a6726402a8286e24f248
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-04-14 20:01:07 -0500

    Add more questions, fixed prob with getting steps

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,134 @@
 // TODO: Include packages needed for this application
+const { verify } = require('crypto');
+const  fs = require('fs');
+const inquirer = require('inquirer');
+
+
+function validateNotEmpty(confirmNotBlank){
+    if(!confirmNotBlank) return false;
+    return true;
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
+let steps = [];
+
+const questions = [
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of your Project? ",
+    validate: confirmNotBlank => {
+        return validateNotEmpty(confirmNotBlank);
+    }
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please enter a brief description of the project: ",
+    validate: confirmNotBlank => {
+        return validateNotEmpty(confirmNotBlank);
+    }
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Explain how to use your program: ",
+    validate: confirmNotBlank => {
+        return validateNotEmpty(confirmNotBlank);
+    }
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Explain how to run the tests for your program (ENTER to skip): "
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Which license is your README using?',
+    choices: ['BSD', 'MIT', 'GPL'],
+},
+{
+    type: "input",
+    name: "githubUsername",
+    message: "Enter your Github Username: ",
+    validate: confirmNotBlank => {
+        return validateNotEmpty(confirmNotBlank);
+    }
+},
+{
+    type: "input",
+    name: "email",
+    message: "Enter your email"
+},
+{
+    type: "input",
+    name: "howToReach",
+    message: "Enter any additional information on how to reach you: (ENTER for none)"
+},
+{
+    type: "number",
+    name: "numberOfSteps",
+    message: "How many steps are there?",
+    validate: confirmNotBlank => {
+        if(!confirmNotBlank) return "Please Enter a number, 0-...";
+        debugger;
+        for(let i = 1; i<=confirmNotBlank; i++){
+            steps.push({
+                type: "input",
+                name: "step" + i,
+                message: "Step " + i + ": ",
+                validate: confirmNotBlank => {
+                    return validateNotEmpty(confirmNotBlank);
+                }
+            });
+        }
+        return true;
+    },
+    filter: input => {
+        // clear the invalid input
+        // if user enters an empty value or number less than 0, return an empty string
+        // else continue
+        return Number.isNaN(input) || Number(input) <= 0 ? '' : Number(input)
+    },
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // begins inquirer question flow
+    return inquirer
+    .prompt(questions)
+    // first round of questions answered
+    .then(answersPart1 => {
+        return getStepsForInstall(steps, answersPart1)
+    })
+    .then(answers => {
+        console.log(answers);
+    })
+
+    // writes the readme file
+    writeToFile("README.md", );
+}
+
+// gets all steps on how to 
+function getStepsForInstall(stepQuestions, answersPart1){
+    // if is a new readme, initialize steps attribute
+    if(!steps) return;
+
+    // begining of inquirer question flow
+    return inquirer
+    .prompt(stepQuestions)
+    .then(answers => {
+        // adds the new step to the attribute queue
+        answersPart1.steps = answers;
+        // return updated question object
+        return answersPart1;
+    })
+}
 
 // Function call to initialize app
 init();
